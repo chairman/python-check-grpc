56298a4d3b67222f6b3f92dda7aef8a98a5b8a20
pingping <805029402@qq.com>
2021-12-26 12:41:22 +0800

    注册服务

--- check_grpc_server.py
+++ check_grpc_server.py
@@ -5,9 +5,24 @@ from nacos.usage import host_ip
 from src.api import hello_pb2,hello_pb2_grpc
 import grpc
 import time
+import _thread
 from concurrent import futures
 import asyncio
-import logging
+import json
+import logging.config
+
+def configure_logging(configure_file_path="LogConfigure.json", default_level=logging.INFO, env_key="LOG_CFG"):
+    path = configure_file_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, "r") as f:
+            config = json.load(f)
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
 import nacos
 NACOS_SERVER_ADDRESSES = "112.74.87.26:8848"
 NACOS_NAMESPACE = "passjava"
@@ -32,11 +47,11 @@ def nacos_send_heartbeat():
             interval = nacos_client.send_heartbeat(server_name,
                     host_ip,rpc_port,cluster_name,1,"{}")
             code = interval['code']
+            logging.info("nacos_send_heartbeat server on %s", code)
         except Exception as e:
             print(e)
 
 class Greeter(hello_pb2_grpc.GreeterServicer):
-
     async def SayHello(
             self, request: hello_pb2.HelloRequest,
             context: grpc.aio.ServicerContext) -> hello_pb2.HelloReply:
@@ -51,10 +66,17 @@ async def serve() -> None:
     await server.start()
     await server.wait_for_termination()
 
+def set_log_info():
+    logging.info("Let's start to log some information.")
+    # logging.error("There are so many errors.")
 
 if __name__ == '__main__':
+    configure_logging("LogConfigure.json")
+    set_log_info()
     logging.basicConfig(level=logging.INFO)
     register_success = nacos_register()
     if not register_success:
-        print("xxxx")
+        print("注册失败")
+    _thread.start_new_thread(nacos_send_heartbeat,())
+    logging.info("init grpc finish ....")
     asyncio.run(serve())
